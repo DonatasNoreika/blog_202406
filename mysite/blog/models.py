dfa9a1360bd8777f827c7d5e9af408610aa04f2f
DonatasNoreika <donoras@gmail.com>
2024-06-07 09:48:26 +0300

    +Aprašytas DetailView

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -17,10 +17,13 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(to="Post", verbose_name="Post", on_delete=models.CASCADE)
+    post = models.ForeignKey(to="Post", verbose_name="Post", on_delete=models.CASCADE, related_name='comments')
     content = models.TextField(verbose_name="Content", max_length=10000)
     author = models.ForeignKey(to=User, verbose_name="Author", on_delete=models.SET_NULL, null=True, blank=True)
     date = models.DateTimeField(verbose_name="Date", auto_now_add=True)
 
     def __str__(self):
         return f"{self.post} - {self.date} ({self.author})"
+
+    class Meta:
+        ordering = ['-date']
