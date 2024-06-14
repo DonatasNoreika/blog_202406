b0adad1c66969171d4e9f5a9ea5a4adebcdca905
DonatasNoreika <donoras@gmail.com>
2024-06-14 09:47:34 +0300

    +Galimybė palikti komentarą prisijungusiam vartotojui prie posto

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.views import generic
 from .models import Post, Comment
 from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import FormMixin
 from django.views.decorators.csrf import csrf_protect
 from django.contrib import messages
 from django.contrib.auth import password_validation
 from django.contrib.auth.forms import User
+from .forms import CommentForm
 
 # Create your views here.
 class PostListView(generic.ListView):
@@ -16,10 +18,31 @@ class PostListView(generic.ListView):
     paginate_by = 5
 
 
-class PostDetailView(generic.DetailView):
+class PostDetailView(FormMixin, generic.DetailView):
     model = Post
     context_object_name = "post"
     template_name = "post.html"
+    form_class = CommentForm
+
+    # nurodome, kur atsidursime komentaro sėkmės atveju.
+    def get_success_url(self):
+        return reverse("post", kwargs={"pk": self.object.id})
+
+    # standartinis post metodo perrašymas, naudojant FormMixin, galite kopijuoti tiesiai į savo projektą.
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    # štai čia nurodome, kad knyga bus būtent ta, po kuria komentuojame, o vartotojas bus tas, kuris yra prisijungęs.
+    def form_valid(self, form):
+        form.instance.post = self.object
+        form.instance.author = self.request.user
+        form.save()
+        return super().form_valid(form)
 
 
 def search(request):
