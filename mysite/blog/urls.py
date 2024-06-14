70fc56073a32536a54f7ad6260f6ef4ea559e528
DonatasNoreika <donoras@gmail.com>
2024-06-14 09:01:08 +0300

    +Įdėti paiešką

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, search
 
 urlpatterns = [
     path('', PostListView.as_view(), name="posts"),
     path('posts/<int:pk>', PostDetailView.as_view(), name="post"),
+    path("search/", search, name="search"),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
+from django.db.models import Q
 
 
 # Create your views here.
@@ -16,3 +17,8 @@ class PostDetailView(generic.DetailView):
     context_object_name = "post"
     template_name = "post.html"
 
+
+def search(request):
+    query = request.GET.get('query')
+    post_search_results = Post.objects.filter(Q(title__icontains=query) | Q(content__icontains=query) | Q(author__username__icontains=query))
+    return render(request, 'search.html', context={"posts": post_search_results, "query": query})
