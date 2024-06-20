39d34185218ef5d82f404a2f9ef4e82d40f25412
DonatasNoreika <donoras@gmail.com>
2024-06-20 09:35:25 +0300

    +Nuotrauka automatiškai pakeičiama į kvadratinę formą. Autoriaus nuotrauka rodoma prie įrašų ir komentarų.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.forms import User
 from tinymce.models import HTMLField
+from PIL import Image
+
 
 # Create your models here.
 class Post(models.Model):
@@ -17,6 +19,7 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-date']
+
 
 class Photo(models.Model):
     link = models.ImageField(verbose_name="Photo", upload_to="photos")
@@ -42,3 +45,34 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username} profile"
 
+    def save(self, *args, **kwargs):
+        super().save()
+        img = Image.open(self.photo.path)
+
+        # When image height is greater than its width
+        if img.height > img.width:
+            # make square by cutting off equal amounts top and bottom
+            left = 0
+            right = img.width
+            top = (img.height - img.width) / 2
+            bottom = (img.height + img.width) / 2
+            img = img.crop((left, top, right, bottom))
+            # Resize the image to 300x300 resolution
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.photo.path)
+
+        # When image width is greater than its height
+        elif img.width > img.height:
+            # make square by cutting off equal amounts left and right
+            left = (img.width - img.height) / 2
+            right = (img.width + img.height) / 2
+            top = 0
+            bottom = img.height
+            img = img.crop((left, top, right, bottom))
+            # Resize the image to 300x300 resolution
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.photo.path)
