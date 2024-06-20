a4f84997093ddee8babc14dba6b3925098ddee01
DonatasNoreika <donoras@gmail.com>
2024-06-20 10:59:00 +0300

    +Modeliai išversti į LT

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.forms import User
 from tinymce.models import HTMLField
 from PIL import Image
-
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(verbose_name="Title", max_length=100)
-    content = HTMLField(verbose_name="Content", max_length=10000)
-    date = models.DateTimeField(verbose_name="Date", auto_now_add=True)
-    author = models.ForeignKey(to=User, verbose_name="Author", on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(verbose_name=_("Title"), max_length=100)
+    content = HTMLField(verbose_name=_("Content"), max_length=10000)
+    date = models.DateTimeField(verbose_name=_("Date"), auto_now_add=True)
+    author = models.ForeignKey(to=User, verbose_name=_("Author"), on_delete=models.SET_NULL, null=True, blank=True)
 
     def comments_count(self):
         return self.comments.all().count()
@@ -19,31 +19,44 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-date']
+        verbose_name = _("Post")
+        verbose_name_plural = _("Posts")
 
 
 class Photo(models.Model):
-    link = models.ImageField(verbose_name="Photo", upload_to="photos")
+    link = models.ImageField(verbose_name=_("Photo"), upload_to="photos")
+
+    def __str__(self):
+        return self.link.path
+
+    class Meta:
+        verbose_name = _("Photo")
+        verbose_name_plural = _("Photos")
+
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(to="Post", verbose_name="Post", on_delete=models.CASCADE, related_name='comments')
-    content = models.TextField(verbose_name="Content", max_length=10000)
-    author = models.ForeignKey(to=User, verbose_name="Author", on_delete=models.SET_NULL, null=True, blank=True)
-    date = models.DateTimeField(verbose_name="Date", auto_now_add=True)
+    post = models.ForeignKey(to="Post", verbose_name=_("Post"), on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(verbose_name=_("Content"), max_length=10000)
+    author = models.ForeignKey(to=User, verbose_name=_("Author"), on_delete=models.SET_NULL, null=True, blank=True)
+    date = models.DateTimeField(verbose_name=_("Date"), auto_now_add=True)
 
     def __str__(self):
         return f"{self.post} - {self.date} ({self.author})"
 
     class Meta:
         ordering = ['-date']
+        verbose_name = _("Comment")
+        verbose_name_plural = _("Comments")
+
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    photo = models.ImageField(default="profile_pics/default.png", upload_to="profile_pics")
+    photo = models.ImageField(verbose_name=_("Photo"), default="profile_pics/default.png", upload_to="profile_pics")
 
     def __str__(self):
-        return f"{self.user.username} profile"
+        return f'{self.user.username} {_("profile")}'
 
     def save(self, *args, **kwargs):
         super().save()
@@ -76,3 +89,7 @@ class Profile(models.Model):
                 output_size = (300, 300)
                 img.thumbnail(output_size)
                 img.save(self.photo.path)
+
+    class Meta:
+        verbose_name = _("Profile")
+        verbose_name_plural = _("Profiles")
